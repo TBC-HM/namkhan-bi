9449048642e41e7b4ad1af43de5a71402ccc925d
PBS-Hospitality <pbsbase@gmail.com>
2026-05-09 22:56:46 +0200

    feat: add /diagnostic/test hello world page

---
+++ app/diagnostic/test/page.tsx
@@ -0,0 +1,7 @@
+export default function DiagnosticTestPage() {
+  return (
+    <main style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh", fontFamily: "sans-serif" }}>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
