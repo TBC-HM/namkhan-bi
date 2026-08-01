2ec3632185dae2e64404e21313d0d1e32d339636
TBC Supabase Bridge <bridge@thebeyondcircle.com>
2026-08-01 23:32:47 +0200

    refactor(it2-r6): repoint imports to lib/cockpit + components/cockpit in page.tsx

--- app/holding/it2/questions/page.tsx
+++ app/holding/it2/questions/page.tsx
@@ -11,7 +11,7 @@
 
 import { revalidatePath } from 'next/cache';
 import { getSupabaseAdmin } from '@/lib/supabaseAdmin';
-import { TOKENS, MONO } from '@/app/holding/it/cockpit/_components/tokens';
+import { TOKENS, MONO } from '@/components/cockpit/tokens';
 import QuestionWalkthrough, { type OpenQ } from './QuestionWalkthrough';
 
 export const dynamic = 'force-dynamic';
